aa7fafcadf53611eb12b59f6f43656d6b6e4f5df
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-01-01 15:22:30 +0300

    adding header image

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import FilterSlice from "./FilterSlice";
+import headerImg from "../public/images/bg-header-desktop.svg";
 
 function Header() {
   return (
-    <div className="bg-desaturated-dark-cyan w-screen h-32 mb-20 relative">
+    <div className="bg-desaturated-dark-cyan w-screen mb-20 relative">
+      <img src={headerImg} alt="" className="w-screen" />
       <FilterSlice></FilterSlice>
     </div>
   );
